6abea050da411396ef3911fc0bb87a89c13d61a5
Rajiv Neupane <htdrajiv@gmail.com>
2020-12-07 17:22:31 -0600

    changed title

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ function Layout({ children }) {
     return (
         <div id="main-wrapper">
             <Head>
-                <title>My page title</title>
+                <title>ERealty</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <div className="">
